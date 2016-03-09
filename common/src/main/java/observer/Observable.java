6237a9759179c8eba3206dbab3b299b11bb6181d
zcy14 <zcy14@software.nju.edu.cn>
2016-03-09 21:59:06 +0800

    Develop: Create Observer/Observable

---
+++ common/src/main/java/observer/Observable.java
@@ -0,0 +1,8 @@
+package observer;
+
+/**
+ * Created by user on 2016/3/9.
+ */
+public interface Observable {
+    public void notify(Object o);
+}
---
+++ common/src/main/java/observer/Observer.java
@@ -0,0 +1,8 @@
+package observer;
+
+/**
+ * Created by user on 2016/3/9.
+ */
+public interface Observer {
+    public void update();
+}
