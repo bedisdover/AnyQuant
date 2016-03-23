6a83f40492fdbec6622ce5998cb474151d3fe052
“zmj14 <zmj14@software.nju.edu.cn>
2016-03-23 14:49:01 +0800

    Development: line chart javafx just beginning

---
+++ client/src/main/java/presentation/javaFXInterface/LineChartInterface.java
@@ -0,0 +1,9 @@
+package presentation.javaFXInterface;
+
+/**
+ * Created by pc on 2016/3/23.
+ */
+public interface LineChartInterface {
+    void addData(String name,double data);
+    void showChart();
+}
---
+++ client/src/main/java/presentation/javaTest/JavaTest.java
@@ -0,0 +1,15 @@
+package presentation.javaTest;/**
+ * Created by pc on 2016/3/23.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.reflect.FXClassType;
+import javafx.reflect.FXLocal;
+import javafx.reflect.FXLocal.Context;
+import javafx.reflect.FXLocal.ObjectValue;
+
+public class JavaTest extends Application {
+
+
+}
