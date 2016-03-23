93e4ba35e651d30bc750f1608692658043372769
zcy14 <zcy14@software.nju.edu.cn>
2016-03-23 23:18:08 +0800

    Develop: trials and errors

--- client/src/test/presentation/ShowGraphs.java
+++ client/src/test/presentation/ShowGraphs.java
@@ -1,8 +1,36 @@
 package presentation;
 
+import java.awt.Font;
+import java.text.SimpleDateFormat;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Month;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+
 /**
  * Created by user on 2016/3/22.
  */
 public class ShowGraphs {
-
+    ChartPanel  panel;
+    public ShowGraphs(){
+        XYDataset xydataset = createDataset();
+        JFreeChart jfreechart = ChartFactory.createTimeSeriesChart("Legal & General单位信托基金价格", "日期", "价格",xydataset, true, true, true);
+        XYPlot xyplot = (XYPlot) jfreechart.getPlot();
+        DateAxis dateaxis = (DateAxis) xyplot.getDomainAxis();
+        dateaxis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+        panel = new ChartPanel(jfreechart,true);
+        dateaxis.setLabelFont(new Font("黑体",Font.BOLD,14));         //水平底部标题
+        dateaxis.setTickLabelFont(new Font("宋体",Font.BOLD,12));  //垂直标题
+        ValueAxis rangeAxis=xyplot.getRangeAxis();//获取柱状
+        rangeAxis.setLabelFont(new Font("黑体",Font.BOLD,15));
+        jfreechart.getLegend().setItemFont(new Font("黑体", Font.BOLD, 15));
+        jfreechart.getTitle().setFont(new Font("宋体", Font.BOLD,20));//设置标题字体
+    }
 }
