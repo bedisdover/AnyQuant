11ec102ddfeecf289d7e5e6ab5635c2019cdc4df
“zmj14 <zmj14@software.nju.edu.cn>
2016-03-26 09:40:23 +0800

    Development:delete javafx

--- client/src/main/java/presentation/javaFXInterface/LineChartInterface.java
+++
@@ -1,9 +0,0 @@
-package presentation.javaFXInterface;
-
-/**
- * Created by pc on 2016/3/23.
- */
-public interface LineChartInterface {
-    void addData(String name,double data);
-    void showChart();
-}
