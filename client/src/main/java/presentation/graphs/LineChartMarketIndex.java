14cf3dd2fe6706385d85952d27c7fb6393f9396f
“zmj14 <zmj14@software.nju.edu.cn>
2016-03-26 17:34:53 +0800

    Development:line chart of market index modify

--- client/src/main/java/presentation/graphs/LineChartMarketIndex.java
+++ client/src/main/java/presentation/graphs/LineChartMarketIndex.java
@@ -8,6 +8,7 @@ import bl.ShowIndexData;
 import org.jfree.chart.ChartPanel;
 
 import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.CategoryPlot;
@@ -25,10 +26,11 @@ import vo.IndexVO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class LineChartMarketIndex {
     ChartPanel panel;
-    TimeSeries timeSeries1=new TimeSeries("", Day.class);
+    TimeSeries timeSeries1 = new TimeSeries("", Day.class);
 
     public LineChartMarketIndex() throws IOException {
         createTimeSeriesChart();
@@ -39,52 +41,58 @@ public class LineChartMarketIndex {
         timeSeriesChart.setBackgroundPaint(Color.WHITE);
         XYPlot plot = timeSeriesChart.getXYPlot();
         setXYPolt(plot);
-        panel = new ChartPanel(timeSeriesChart,true);
+        panel = new ChartPanel(timeSeriesChart, true);
         LegendTitle legendTitle = timeSeriesChart.getLegend();
         legendTitle.setVisible(false);
-        Font font =new Font("宋体",Font.BOLD,16);
-        TextTitle title=new TextTitle("大盘指数",font);//副标题 
-    //    TextTitle subtitle=new TextTitle("副标题", new Font("黑体",Font.BOLD,12));
-     //   timeSeriesChart.addSubtitle(subtitle);
+        Font font = new Font("宋体", Font.BOLD, 16);
+        TextTitle title = new TextTitle("大盘指数", font);//副标题 
+        //    TextTitle subtitle=new TextTitle("副标题", new Font("黑体",Font.BOLD,12));
+        //   timeSeriesChart.addSubtitle(subtitle);
         timeSeriesChart.setTitle(title);//标题
-        NumberAxis numberAxis=(NumberAxis)plot.getRangeAxis();
+        NumberAxis numberAxis = (NumberAxis) plot.getRangeAxis();
         ValueAxis domainAxis = plot.getDomainAxis();
 
         /*------设置X轴坐标上的文字-----------*/
-        domainAxis.setTickLabelFont(new Font("sans-serif",Font.PLAIN,11));
+        domainAxis.setTickLabelFont(new Font("sans-serif", Font.PLAIN, 12));
         /*------设置X轴的标题文字------------*/
-        domainAxis.setLabelFont(new Font("黑体",Font.PLAIN,18));
+        domainAxis.setLabelFont(new Font("黑体", Font.PLAIN, 18));
         /*------设置Y轴坐标上的文字-----------*/
-        numberAxis.setTickLabelFont(new Font("sans-serif",Font.PLAIN,12));
+        numberAxis.setTickLabelFont(new Font("sans-serif", Font.PLAIN, 12));
         /*------设置Y轴的标题文字------------*/
-        numberAxis.setLabelFont(new Font("黑体",Font.PLAIN,18));
-
-
-
+        numberAxis.setLabelFont(new Font("黑体", Font.PLAIN, 18));
+        // x轴 // 分类轴网格是否可见
+        plot.setDomainGridlinesVisible(true);
+        // y轴 //数据轴网格是否可见
+        plot.setRangeGridlinesVisible(true);
+        // 设置网格背景颜色
+        plot.setBackgroundPaint(Color.white);
+        // 设置网格竖线颜色
+        plot.setDomainGridlinePaint(Color.pink);
+        // 设置网格横线颜色
+        plot.setRangeGridlinePaint(Color.pink);
+        //设置日期显示格式
+        DateAxis dateaxiss=(DateAxis)plot.getDomainAxis();
+        SimpleDateFormat frm = new SimpleDateFormat("MM.dd");
     }
 
     public XYDataset createDataset() throws IOException {
 
         TimeSeriesCollection dataset = new TimeSeriesCollection();
         IndexVO index = new ShowIndexData().getLatestIndexData();
-        int length=index.getDate().length;
-    for(int i=1;i<=length;i++){
-        int year=Integer.parseInt(index.getDate()[length-i].substring(0,4));
-        int month=Integer.parseInt(index.getDate()[length-i].substring(5,7));
-        int day=Integer.parseInt(index.getDate()[length-i].substring(8,10));
-        Day d = new Day(day, month, year);
-        timeSeries1.add(d,index.getVolume()[length-i]);
+        int length = index.getDate().length;
+        for (int i = 1; i <= length; i++) {
+            int year = Integer.parseInt(index.getDate()[length - i].substring(0, 4));
+            int month = Integer.parseInt(index.getDate()[length - i].substring(5, 7));
+            int day = Integer.parseInt(index.getDate()[length - i].substring(8, 10));
+            Day d = new Day(day, month, year);
+            timeSeries1.add(d, index.getVolume()[length - i]);
 
-        dataset.addSeries(timeSeries1);
-    }
-
-
-
-
+            dataset.addSeries(timeSeries1);
+        }
         return dataset;
     }
 
-    public void setXYPolt(XYPlot xyPolt){
+    public void setXYPolt(XYPlot xyPolt) {
         xyPolt.setDomainGridlinePaint(Color.LIGHT_GRAY);
         xyPolt.setRangeGridlinePaint(Color.LIGHT_GRAY);
         XYItemRenderer r = xyPolt.getRenderer();
@@ -95,7 +103,7 @@ public class LineChartMarketIndex {
         }
     }
 
-    public ChartPanel getChartPanel(){
+    public ChartPanel getChartPanel() {
         return panel;
     }
 
@@ -105,8 +113,6 @@ public class LineChartMarketIndex {
         jFrame.setBounds(50, 50, 800, 600);
         jFrame.setVisible(true);
         LineChartMarketIndex g = new LineChartMarketIndex();
-
-
     }
 
 
