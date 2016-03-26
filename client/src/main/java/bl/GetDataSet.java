878f15858ae39109ccdd8df2cd53633a4ceb2b58
zcy14 <zcy14@software.nju.edu.cn>
2016-03-26 10:15:39 +0800

    Develop: Draw KLine

--- client/src/main/java/bl/GetDataSet.java
+++ client/src/main/java/bl/GetDataSet.java
@@ -1,7 +1,13 @@
 package bl;
 
 import blservice.GetDataSetService;
+import data.GetIndexData;
+import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
+import po.IndexPO;
+import vo.IndexVO;
+
+import java.io.IOException;
 
 /**
  * Created by user on 2016/3/24.
@@ -18,8 +24,12 @@ public class GetDataSet implements GetDataSetService{
     }
 
 
-    public XYDataset getIndexDataSet_monthly() {
-        return null;
+    public IndexVO getIndexDataSet() throws IOException {
+        GetIndexData getIndexData = new GetIndexData();
+        IndexPO indexPO = getIndexData.getLatestIndexData();
+        IndexVO indexVO = new IndexVO(indexPO);
+
+        return indexVO;
     }
 
 
--- client/src/main/java/blservice/GetDataSetService.java
+++ client/src/main/java/blservice/GetDataSetService.java
@@ -1,6 +1,9 @@
 package blservice;
 
 import org.jfree.data.xy.XYDataset;
+import vo.IndexVO;
+
+import java.io.IOException;
 
 /**
  * Created by user on 2016/3/24.
@@ -8,6 +11,6 @@ import org.jfree.data.xy.XYDataset;
 public interface GetDataSetService {
     public XYDataset getStockDataSet_monthly();
     public XYDataset getStockDataSet_daily();
-    public XYDataset getIndexDataSet_monthly();
+    public IndexVO getIndexDataSet() throws IOException;
     public XYDataset getIndexDataSet_daily();
 }
