b4c8025b436215218e10354c933dac71bc482e6c
“zmj14 <zmj14@software.nju.edu.cn>
2016-03-26 17:05:06 +0800

    Development:line chart of market index

--- client/src/main/java/presentation/graphs/LineChartMarketIndex.java
+++ client/src/main/java/presentation/graphs/LineChartMarketIndex.java
@@ -7,12 +7,17 @@ package presentation.graphs;
 import bl.ShowIndexData;
 import org.jfree.chart.ChartPanel;
 
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.title.LegendTitle;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.time.*;
 import org.jfree.data.xy.XYDataset;
 import vo.IndexVO;
@@ -23,38 +28,59 @@ import java.io.IOException;
 
 public class LineChartMarketIndex {
     ChartPanel panel;
+    TimeSeries timeSeries1=new TimeSeries("", Day.class);
 
-
-    public LineChartMarketIndex(){
+    public LineChartMarketIndex() throws IOException {
         createTimeSeriesChart();
     }
 
-    public void createTimeSeriesChart() {
-        JFreeChart timeSeriesChart = ChartFactory.createTimeSeriesChart("大盘指数", "日期", "成交量", createDataset(), true, true, false);
+    public void createTimeSeriesChart() throws IOException {
+        JFreeChart timeSeriesChart = ChartFactory.createTimeSeriesChart("", "日期", "成交量", createDataset(), true, true, false);
         timeSeriesChart.setBackgroundPaint(Color.WHITE);
         XYPlot plot = timeSeriesChart.getXYPlot();
         setXYPolt(plot);
         panel = new ChartPanel(timeSeriesChart,true);
         LegendTitle legendTitle = timeSeriesChart.getLegend();
         legendTitle.setVisible(false);
+        Font font =new Font("宋体",Font.BOLD,16);
+        TextTitle title=new TextTitle("大盘指数",font);//副标题 
+    //    TextTitle subtitle=new TextTitle("副标题", new Font("黑体",Font.BOLD,12));
+     //   timeSeriesChart.addSubtitle(subtitle);
+        timeSeriesChart.setTitle(title);//标题
+        NumberAxis numberAxis=(NumberAxis)plot.getRangeAxis();
+        ValueAxis domainAxis = plot.getDomainAxis();
+
+        /*------设置X轴坐标上的文字-----------*/
+        domainAxis.setTickLabelFont(new Font("sans-serif",Font.PLAIN,11));
+        /*------设置X轴的标题文字------------*/
+        domainAxis.setLabelFont(new Font("黑体",Font.PLAIN,18));
+        /*------设置Y轴坐标上的文字-----------*/
+        numberAxis.setTickLabelFont(new Font("sans-serif",Font.PLAIN,12));
+        /*------设置Y轴的标题文字------------*/
+        numberAxis.setLabelFont(new Font("黑体",Font.PLAIN,18));
+
+
 
     }
 
     public XYDataset createDataset() throws IOException {
 
         TimeSeriesCollection dataset = new TimeSeriesCollection();
-        IndexVO index = null;
+        IndexVO index = new ShowIndexData().getLatestIndexData();
+        int length=index.getDate().length;
+    for(int i=1;i<=length;i++){
+        int year=Integer.parseInt(index.getDate()[length-i].substring(0,4));
+        int month=Integer.parseInt(index.getDate()[length-i].substring(5,7));
+        int day=Integer.parseInt(index.getDate()[length-i].substring(8,10));
+        Day d = new Day(day, month, year);
+        timeSeries1.add(d,index.getVolume()[length-i]);
 
-        index = new ShowIndexData().getLatestIndexData();
-TimeSeries timeSeries1=new TimeSeries();
-        timeSeries1.add(index.getDate(),index.getVolume());
+        dataset.addSeries(timeSeries1);
+    }
 
-//        TimeSeries timeSeries1 = new TimeSeries("", Minute.class);
-//
-//
-//        timeSeries1.add(new Minute(40, hour10), 11.79);
-//        timeSeries1.add(new Minute(45, hour10), 11.72);
-       dataset.addSeries(timeSeries1);
+
+
+
         return dataset;
     }
 
@@ -73,12 +99,14 @@ TimeSeries timeSeries1=new TimeSeries();
         return panel;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         JFrame jFrame = new JFrame();
         jFrame.add(new LineChartMarketIndex().getChartPanel());
         jFrame.setBounds(50, 50, 800, 600);
         jFrame.setVisible(true);
         LineChartMarketIndex g = new LineChartMarketIndex();
+
+
     }
 
 
