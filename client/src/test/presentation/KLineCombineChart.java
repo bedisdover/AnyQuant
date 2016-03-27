01db08dc7761a6b2b766957ea740d39cc86a90e2
syz14 <you@example.comsyz14@software.nju.edu.cn>
2016-03-27 23:02:23 +0800

    Fix bug:KLineChart

--- client/src/test/presentation/KLineCombineChart.java
+++ client/src/test/presentation/KLineCombineChart.java
@@ -117,7 +117,7 @@ public class KLineCombineChart {
         final CandlestickRenderer candlestickRender=new CandlestickRenderer();// 设置K线图的画图器，必须申明为final，后面要在匿名内部类里面用到
         candlestickRender.setUseOutlinePaint(true); // 设置是否使用自定义的边框线，程序自带的边框线的颜色不符合中国股票市场的习惯
         candlestickRender.setAutoWidthMethod(CandlestickRenderer.WIDTHMETHOD_AVERAGE);// 设置如何对K线图的宽度进行设定
-        candlestickRender.setAutoWidthGap(0.001);// 设置各个K线图之间的间隔
+        candlestickRender.setAutoWidthGap(5);// 设置各个K线图之间的间隔
         candlestickRender.setUpPaint(Color.RED);// 设置股票上涨的K线图颜色
         candlestickRender.setDownPaint(Color.GREEN);// 设置股票下跌的K线图颜色
         DateAxis x1Axis=new DateAxis();// 设置x轴，也就是时间轴
