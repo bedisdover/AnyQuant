5ee49680611152a3e5bc3e06ca432c05df10c3eb
“zmj14 <zmj14@software.nju.edu.cn>
2016-03-31 22:57:32 +0800

    Development:line chart parent method done

--- client/src/main/java/presentation/graphs/LineChartMarketIndex.java
+++ client/src/main/java/presentation/graphs/LineChartMarketIndex.java
@@ -1,51 +1,31 @@
 package presentation.graphs;
 
 /**
- * Created by pc on 2016/3/26.
+ * Created by zmj on 2016/3/26.
  */
 
 import bl.ShowIndexData;
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.title.LegendTitle;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.XYDataset;
-import presentation.frame.MainFrame;
-import presentation.panel.info.IndexDataPanel;
 import vo.IndexVO;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Arc2D;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 
-public class LineChartMarketIndex extends LineChartParent {
+public class LineChartMarketIndex {
    public static  LineChartParent lineChartParent;
-public LineChartMarketIndex(){
+public  LineChartMarketIndex(){
     String name[]={"日期","成交量","大盘指数"};
 
     try {
         IndexVO index = new ShowIndexData().getLatestIndexData();
-        String dateString[]=index.getDate();
+        String date[]=index.getDate();
         long volumeLong[]=index.getVolume();
-
-        double date[]=new double[dateString.length];
+//System.out.println(dateString[0]+"看看你是什么鬼");
         double volume[]=new double[volumeLong.length];
-        for(int i=0;i<dateString.length;i++){
-            date[i]=Double.parseDouble(dateString[i]);
+        for(int i=0;i<date.length;i++){
             volume[i]= (double)volumeLong[i];
         }
+//        System.out.println(date[0]+"你又是什么鬼");
         lineChartParent=new LineChartParent(name,date,volume);
     } catch (IOException e) {
         e.printStackTrace();
