537f890b496b59d00631541c882e8e1446f5d9e2
sym14 <sym14@software.nju.edu.cn>
2016-04-02 09:24:18 +0800

    Develop: index info

--- client/src/main/java/presentation/panel/info/IndexBriefInfoPanel.java
+++ client/src/main/java/presentation/panel/info/IndexBriefInfoPanel.java
@@ -1,5 +1,7 @@
 package presentation.panel.info;
 
+import vo.IndexVO;
+
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
@@ -8,20 +10,132 @@ import java.awt.*;
  * Created by 宋益明 on 16-3-31.
  *
  * 大盘指数简要信息面板
+ * 包含
+ *      中文名称                        今开      最高     成交量
+ *        ID        涨跌额（涨跌幅）      昨收     最低     成交额
  */
 public class IndexBriefInfoPanel extends JPanel {
 
+    /**
+     * 大盘指数值对象
+     */
+    private IndexVO index;
+
+    /**
+     * 当前价格
+     */
+    private JLabel labelPrice;
+
+    /**
+     * 涨跌额（涨跌幅）
+     */
+    private JLabel labelIncrease;
+
+    /**
+     * 开盘价
+     */
+    private JLabel labelOpen;
+
+    /**
+     * 收盘价（昨天）
+     */
+    private JLabel labelClose;
+
+    /**
+     * 最高价
+     */
+    private JLabel labelHigh;
+
+    /**
+     * 最低价
+     */
+    private JLabel labelLow;
+
+    /**
+     * 成交量
+     */
+    private JLabel labelVolume;
+
+    /**
+     * 成交额
+     */
+    private JLabel labelNumber;
+
+    public IndexBriefInfoPanel(IndexVO index) {
+        this.index = index;
+
+        init();
+        createUIComponents();
+        setText();
+    }
+
+    /**
+     * 初始化
+     */
+    private void init() {
+        setLayout(null);
+        setBorder(new BevelBorder(BevelBorder.LOWERED));
+        setBackground(new Color(175, 152, 139, 56));
+    }
+
+    /**
+     * 创建组件
+     */
+    private void createUIComponents() {
+        //TODO id
+        IndexNamePanel namePanel = new IndexNamePanel("沪深300", "1B0300");
+        namePanel.setBounds(0, 0, 100, 100);
+
+        labelPrice = new JLabel();
+        labelIncrease = new JLabel();
+        labelOpen = new JLabel();
+        labelClose = new JLabel();
+        labelHigh = new JLabel();
+        labelLow = new JLabel();
+        labelVolume = new JLabel();
+        labelNumber = new JLabel();
+
+        add(namePanel);
+    }
+
+    /**
+     * 设置各种label的文本及颜色
+     * 名称和涨跌额（涨跌幅）无需设置中文文本，需设置提示文本
+     */
+    private void setText() {
+
+        labelPrice.setToolTipText("当前股价");
+        labelIncrease.setToolTipText("涨跌额（涨跌幅）");
+
+        //TODO 涨跌幅
+//        if (index.ge) {
+//            setTextColor(Color.red);
+//            setTextColor(Color.green);
+//        } else {
+//            setTextColor(Color.black);
+//        }
+    }
+
+    /**
+     * 设置文本颜色
+     *
+     * @param color 颜色
+     */
+    private void setTextColor(Color color) {
+        labelPrice.setForeground(color);
+        labelIncrease.setForeground(color);
+    }
 }
 
 /**
  * 大盘指数名称面板
- * 包含中文名称与代码
+ * 包含中文名称与ID
  */
 class IndexNamePanel extends JPanel {
     IndexNamePanel(String name, String id) {
-        setLayout(new FlowLayout(FlowLayout.LEADING, 30, 10));
-        setBorder(new BevelBorder(BevelBorder.RAISED,
-                Color.black, Color.white, Color.red, Color.blue));
+        setBackground(new Color(140, 175, 146, 58));
+        setLayout(null);
+        setBorder(new BevelBorder(BevelBorder.RAISED));
 
         createUIComponents(name, id);
     }
@@ -30,7 +144,7 @@ class IndexNamePanel extends JPanel {
         JLabel labelName = new JLabel(name);
         JLabel labelID = new JLabel(id);
 
-//        labelName.setBounds();
+        labelName.setBounds(10, 10, 60, 20);
 
         add(labelName);
         add(labelID);
--- client/src/main/java/presentation/panel/operation/MarketIndexPanel.java
+++ client/src/main/java/presentation/panel/operation/MarketIndexPanel.java
@@ -1,10 +1,13 @@
 package presentation.panel.operation;
 
+import po.IndexPO;
 import presentation.UltraSwing.UltraButton;
 import presentation.frame.MainFrame;
 import presentation.graphs.LineChartMarketIndex;
+import presentation.panel.info.IndexBriefInfoPanel;
 import presentation.panel.info.IndexInfoPanel;
 import presentation.util.DateChooser;
+import vo.IndexVO;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,6 +20,9 @@ import java.awt.event.MouseEvent;
  * Created by 宋益明 on 16-3-2.
  * <p>
  * 大盘指数面板
+ * 包含简要信息（中文名称                        今开     最高     成交量)
+ *           (  ID        涨跌额（涨跌幅）      昨收     最低     成交额)
+ *    及k-线图
  */
 public class MarketIndexPanel extends OperationPanel {
 
@@ -24,6 +30,11 @@ public class MarketIndexPanel extends OperationPanel {
      * 显示详细数据按钮
      */
     private UltraButton btnData;
+
+    /**
+     * 简要信息面板
+     */
+    private JPanel briefInfoPanel;
 
     /**
      * 图表面板
@@ -43,10 +54,12 @@ public class MarketIndexPanel extends OperationPanel {
     protected void createUIComponents() {
         new DateChooser(this, MARGIN, MARGIN, BUTTON_WIDTH * 2, BUTTON_HEIGHT);
 
-        chartPanel = new LineChartMarketIndex().getChartPanel();
+        briefInfoPanel = new IndexBriefInfoPanel(new IndexVO(new IndexPO(3)));
+//        chartPanel = new LineChartMarketIndex().getChartPanel();
         btnData = new UltraButton("详细数据");
 
-        add(chartPanel);
+        add(briefInfoPanel);
+//        add(chartPanel);
         add(btnData);
     }
 
@@ -59,8 +72,10 @@ public class MarketIndexPanel extends OperationPanel {
             public void componentResized(ComponentEvent e) {
                 btnData.setBounds(PANEL_WIDTH - MARGIN * 2 - BUTTON_WIDTH, MARGIN,
                         BUTTON_WIDTH + MARGIN, BUTTON_HEIGHT);
-                chartPanel.setBounds(MARGIN, MARGIN + BUTTON_HEIGHT + PADDING,
-                        PANEL_WIDTH - MARGIN * 2, PANEL_HEIGHT - getX() - MARGIN);
+                briefInfoPanel.setBounds(MARGIN, MARGIN + BUTTON_HEIGHT + PADDING / 2,
+                        PANEL_WIDTH - MARGIN * 2, BUTTON_HEIGHT + PADDING);
+//                chartPanel.setBounds(MARGIN, MARGIN + BUTTON_HEIGHT + PADDING,
+//                        PANEL_WIDTH - MARGIN * 2, PANEL_HEIGHT - getX() - MARGIN);
             }
         });
 
