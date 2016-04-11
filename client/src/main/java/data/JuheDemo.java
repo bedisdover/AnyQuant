0292158b6a74eb3f9212927b4ef6f58b6e41e1d8
syz14 <you@example.comsyz14@software.nju.edu.cn>
2016-04-11 20:57:21 +0800

    Develop:time series

--- client/src/main/java/data/JuheDemo.java
+++ client/src/main/java/data/JuheDemo.java
@@ -11,107 +11,101 @@ import net.sf.json.JSONObject;
  * Created by syz on 2016/4/9.
  */
 public class JuheDemo {
-    public static final String DEF_CHATSET = "UTF-8";
-    public static final int DEF_CONN_TIMEOUT = 30000;
-    public static String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
+    public final String DEF_CHATSET = "UTF-8";
+    public final int DEF_CONN_TIMEOUT = 30000;
+    public String userAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
 
-    public static final String APPKEY = "5cd97982c4a9f8352b5497694f26be84";
+    public final String APPKEY = "5cd97982c4a9f8352b5497694f26be84";
 
-    public static String getRequest1(String stockName){
+    public String getRequest1(String stockName) {
         String result = null;
         String url = "http://op.juhe.cn/onebox/stock/query";
         Map params = new HashMap();
-        params.put("key",APPKEY);
-        params.put("dtype","");
-        params.put("stock",stockName);
+        params.put("key", APPKEY);
+        params.put("dtype", "");
+        params.put("stock", stockName);
 
-        try{
-            result = net(url, params ,"GET");
+        try {
+            result = net(url, params, "GET");
             JSONObject object = JSONObject.fromObject(result);
-            if(object.getInt("error_code")==0){
-                System.out.println(object.get("result"));
-            }else{
-                System.out.println(object.get("error_code")+":"+object.get("reason"));
+            if (object.getInt("error_code") != 0) {
+                System.out.println(object.get("error_code") + ":" + object.get("reason"));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
     }
 
-    public static void main(String[] args){
-        String result = getRequest1("中国银行");
-//        System.out.println(result);
-        String[] str = result.split("\\{");
-        for(int i = 2;i<str.length;i++){
-            if(str[i].startsWith("\""))
-                System.out.println(str[i].startsWith("\""));
-                 System.out.println(str[i]);
-        }
-//        System.out.println(str[0]);
-//        System.out.println(str[0].startsWith("\""));
-    }
 
-    public static String net(String strUrl,Map params,String method) throws Exception{
+    public String net(String strUrl, Map params, String method) throws Exception {
         HttpURLConnection conn = null;
         BufferedReader reader = null;
         String rs = null;
-        try{
+        try {
             StringBuffer sb = new StringBuffer();
-            if(method==null||method.equals("GET")){
-                strUrl = strUrl+"?"+urlencode(params);
+            if (method == null || method.equals("GET")) {
+                strUrl = strUrl + "?" + urlencode(params);
             }
             URL url = new URL(strUrl);
-            conn = (HttpURLConnection)url.openConnection();
-            if(method==null||method.equals("GET")){
+            conn = (HttpURLConnection) url.openConnection();
+            if (method == null || method.equals("GET")) {
                 conn.setRequestMethod("GET");
-            }else{
+            } else {
                 conn.setRequestMethod("POST");
                 conn.setDoOutput(true);
             }
-            conn.setRequestProperty("User-agent",userAgent);
+            conn.setRequestProperty("User-agent", userAgent);
             conn.setUseCaches(false);
             conn.setConnectTimeout(DEF_CONN_TIMEOUT);
             conn.setReadTimeout((DEF_CONN_TIMEOUT));
             conn.setInstanceFollowRedirects(false);
             conn.connect();
-            if(params!=null&&method.equals("POST")){
-                try{
+            if (params != null && method.equals("POST")) {
+                try {
                     DataOutputStream out = new DataOutputStream(conn.getOutputStream());
                     out.writeByte(Integer.parseInt(urlencode(params)));
-                }catch (Exception e){
+                } catch (Exception e) {
 
                 }
             }
             InputStream is = conn.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(is,DEF_CHATSET));
+            reader = new BufferedReader(new InputStreamReader(is, DEF_CHATSET));
             String strRead = null;
-            while((strRead = reader.readLine())!=null){
+            while ((strRead = reader.readLine()) != null) {
                 sb.append(strRead);
             }
             rs = sb.toString();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if(reader!=null){
+        } finally {
+            if (reader != null) {
                 reader.close();
             }
-            if(conn!=null){
+            if (conn != null) {
                 conn.disconnect();
             }
         }
         return rs;
     }
 
-    public static String urlencode(Map<String,Object>data){
+    public String urlencode(Map<String, Object> data) {
         StringBuilder sb = new StringBuilder();
-        for(Map.Entry i : data.entrySet()){
-            try{
-                sb.append(i.getKey()).append("=").append(URLEncoder.encode(i.getValue()+"","UTF-8")).append("&");
-            }catch(UnsupportedEncodingException e){
+        for (Map.Entry i : data.entrySet()) {
+            try {
+                sb.append(i.getKey()).append("=").append(URLEncoder.encode(i.getValue() + "", "UTF-8")).append("&");
+            } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
         }
         return sb.toString();
     }
+
+    public static void main(String[] args) {
+        JuheDemo j = new JuheDemo();
+        String result = j.getRequest1("中国银行");
+        ReadData rd = new ReadData();
+        String t = rd.parseJSON(result,"result");
+    }
 }
+
