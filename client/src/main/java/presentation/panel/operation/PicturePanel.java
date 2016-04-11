acafb0bda25cfaa94893f7ac8db2856755e12136
sym14 <sym14@software.nju.edu.cn>
2016-04-11 20:21:43 +0800

    Develop: value

--- client/src/main/java/presentation/panel/operation/PicturePanel.java
+++ client/src/main/java/presentation/panel/operation/PicturePanel.java
@@ -289,7 +289,8 @@ public class PicturePanel extends OperationPanel {
             data[i] = new Object[]{
                     ++i, stock.getName(), stock.getId(),
                     stock.getIncrease_decreaseNum()[0],
-                    stock.getIncrease_decreaseRate()[0] * 100 + "%",
+                    ((double) Math.round(stock.getIncrease_decreaseRate()[stock.getDate().length - 1]
+                            * 100 * 100)) / 100 + "%",
                     stock.getVolume()[0], stock.getPb()[0],
                     stock.getHigh()[0], stock.getLow()[0],
                     stock.getPe_ttm()[0], stock.getAdj_price()[0],
