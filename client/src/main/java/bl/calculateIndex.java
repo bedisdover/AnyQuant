433343cc513e855080910f854d6a33917ec2e54c
zcy14 <zcy14@software.nju.edu.cn>
2016-04-13 18:54:52 +0800

    Develop: all kinds of index

---
+++ client/src/main/java/bl/calculateIndex.java
@@ -0,0 +1,8 @@
+package bl;
+
+/**
+ * Created by user on 2016/4/13.
+ */
+public class calculateIndex {
+
+}
---
+++ client/src/main/java/vo/StockIndexVO.java
@@ -0,0 +1,12 @@
+package vo;
+
+/**
+ * Created by zcy on 2016/4/13.
+ * 股票的各项指标
+ */
+public class StockIndexVO {
+    /**
+     * 股票乖离率
+     */
+    double bias;
+}
