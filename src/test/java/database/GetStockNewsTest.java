4d58cd36e22cf94d2d5d489304f0d5a96f26126e
zcy14 <zcy14@software.nju.edu.cn>
2016-05-09 13:32:16 +0800

    Other:pom.xml

--- src/test/java/database/GetStockNewsTest.java
+++ src/test/java/database/GetStockNewsTest.java
@@ -16,4 +16,11 @@ public class GetStockNewsTest {
         List<String> results = getStockNews.getNewsTitle("sh600015");
         assertEquals("华夏银行:资本压力限制规模扩张",results.get(0));
     }
+
+    @Test
+    public void getNewsContentTest(){
+        GetStockNews getStockNews = new GetStockNews();
+        List<String> results = getStockNews.getNewsContent("sh600015");
+        assertEquals("华夏银行:资本压力限制规模扩张",results.get(0));
+    }
 }
