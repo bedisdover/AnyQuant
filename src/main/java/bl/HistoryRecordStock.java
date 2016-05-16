c553ff7565be8f2abb754fe4a6963c409910ca45
zcy14 <zcy14@software.nju.edu.cn>
2016-05-16 20:07:44 +0800

    Develop:selfselectstockmanage

--- src/main/java/bl/HistoryRecordStock.java
+++ src/main/java/bl/HistoryRecordStock.java
@@ -9,76 +9,28 @@ import java.util.List;
 
 /**
  * Created by syz on 2016/3/11.
+ *
  */
 public class HistoryRecordStock implements HistoryRecordStockService {
 
-    private File file;
-
-    private List<String> list;
-
-    public HistoryRecordStock() {
-        file = new File("src/main/resources/historyRecord.data");
-        list = new ArrayList<>();
-
-        load();
-    }
 
     @Override
     public void addStock(String id) throws IOException {
-        if (!exist(id)) {
-            list.add(id);
-            store();
-        }
+
     }
 
     @Override
     public void removeStock(String id) throws IOException {
-        if (exist(id)) {
-            list.remove(id);
-            store();
-        }
+
     }
 
     @Override
     public Iterator<String> getRecord() {
-        return list.iterator();
+        return null;
     }
 
     @Override
     public boolean exist(String id) {
-        return list.contains(id);
-    }
-
-    /**
-     * 加载股票代码列表
-     */
-    private void load() {
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            String stockID;
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            while ((stockID = br.readLine()) != null) {
-                list.add(stockID);
-            }
-
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * 存储更新后的股票列表
-     */
-    private void store() throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-
-        for (String stockID : list) {
-            bw.write(stockID + "\n");
-        }
-        bw.close();
+        return false;
     }
 }
