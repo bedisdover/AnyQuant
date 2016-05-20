92eaeb66ed98d1ed392419402189e5f94b94952f
zcy14 <zcy14@software.nju.edu.cn>
2016-05-20 14:50:34 +0800

    Develop:sortstock modify

--- src/main/java/bl/SortStock.java
+++ src/main/java/bl/SortStock.java
@@ -42,6 +42,89 @@ public class SortStock implements SortStockService{
         }
     }
 
+    /**
+     * @param n 1表示按涨幅榜排序，2表示按成交量榜排序
+     * @return List<StockVO>
+     * @throws IOException
+     * 得到排好序之后的股票列表
+     */
+    public List<StockVO> getResultList(int n) throws IOException {
+        List<StockVO> result = new ArrayList<>();
+        int length;
+        long[] volume = new long[1];
+        double[] pb = new double[1];
+        double[] high = new double[1];
+        double[] pe_ttm = new double[1];
+        double[] adj_price = new double[1];
+        double[] low = new double[1];
+        String[] date = new String[1];
+        double[] close = new double[1];
+        double[] open = new double[1];
+        double[] turnover = new double[1];
+        if(n==1){
+            List<StockVO> list = increase_sort();
+            length = list.get(0).getDate().length;
+            for(int i=0;i<list.size();i++){
+                StockPO stockPO = new StockPO(1);
+                volume[0] = list.get(i).getVolume()[length-1];
+                pb[0] = list.get(i).getPb()[length-1];
+                high[0] = list.get(i).getHigh()[length-1];
+                pe_ttm[0] = list.get(i).getPe_ttm()[length-1];
+                adj_price[0] = list.get(i).getAdj_price()[length-1];
+                low[0] = list.get(i).getLow()[length-1];
+                date[0] = list.get(i).getDate()[length-1];
+                close[0] = list.get(i).getClose()[length-1];
+                open[0] = list.get(i).getOpen()[length-1];
+                turnover[0] = list.get(i).getTurnover()[length-1];
+                stockPO.setId(list.get(i).getId());
+                stockPO.setVolume(volume);
+                stockPO.setPb(pb);
+                stockPO.setHigh(high);
+                stockPO.setPe_ttm(pe_ttm);
+                stockPO.setAdj_price(adj_price);
+                stockPO.setLow(low);
+                stockPO.setDate(date);
+                stockPO.setClose(close);
+                stockPO.setOpen(open);
+                stockPO.setTurnover(turnover);
+                StockVO stockVO = new StockVO(stockPO);
+                list.add(stockVO);
+            }
+            return list;
+        }
+        else{
+            List<StockVO> list = volume_sort();
+            length = list.get(0).getDate().length;
+            for(int i=0;i<list.size();i++){
+                StockPO stockPO = new StockPO(1);
+                volume[0] = list.get(i).getVolume()[length-1];
+                pb[0] = list.get(i).getPb()[length-1];
+                high[0] = list.get(i).getHigh()[length-1];
+                pe_ttm[0] = list.get(i).getPe_ttm()[length-1];
+                adj_price[0] = list.get(i).getAdj_price()[length-1];
+                low[0] = list.get(i).getLow()[length-1];
+                date[0] = list.get(i).getDate()[length-1];
+                close[0] = list.get(i).getClose()[length-1];
+                open[0] = list.get(i).getOpen()[length-1];
+                turnover[0] = list.get(i).getTurnover()[length-1];
+                stockPO.setVolume(volume);
+                stockPO.setPb(pb);
+                stockPO.setHigh(high);
+                stockPO.setPe_ttm(pe_ttm);
+                stockPO.setAdj_price(adj_price);
+                stockPO.setLow(low);
+                stockPO.setDate(date);
+                stockPO.setClose(close);
+                stockPO.setOpen(open);
+                stockPO.setTurnover(turnover);
+                StockVO stockVO = new StockVO(stockPO);
+                stockPO.setId(list.get(i).getId());
+                list.add(stockVO);
+            }
+            return list;
+        }
+    }
+
     public List<StockVO> getStockVOs() {
         return stockVOs;
     }
